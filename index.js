37f143ab708c70fd069067eb0b6fff970ec984ad
NefertariLaurel <laurel_04@outlook.com>
2023-08-11 04:00:14 -0600

    Long and size

--- index.js
+++ index.js
@@ -1,29 +1,7 @@
-let repited = listOfNames.map((el)=>0);
-listOfNames.forEach((name)=>{
-    listOfNames.forEach((el,i)=>{
-        if(el === name ){
-            repited[i]++;
-        }
-    })
-})
+const duplicatedNames = (arrayNames)=> {
+    let duplicados = arrayNames.filter((value,index,array)=>{
+        return array.indexOf(value) !== index;
+    });
 
-let isRepited = repited.reduce ((acum,curr)=>{
-    return curr > 1 ? true : false;
-},false)
-console.log(isRepited ? 'se repite': 'no se repite')
-
-
-/* cual es el nombre mas largo */
-
-let nameSize = listOfNames.reduce((acum,curr)=>{
-    return curr.length > acum.length ? curr : acum ;
-}, listOfNames[0])
-console.log(nameSize)
-
-
-/* cual es el nombre mas corto */
-
-let nameShort = listOfNames.reduce((acum,curr)=>{
-    return curr.length < acum.length ? curr : acum ;
-}, listOfNames[0])
-console.log(nameShort)
+    return duplicados.length > 0;
+}
