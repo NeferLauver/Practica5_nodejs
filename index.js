7a78b5ca8e0b759545a38fb8722be5076e692d8e
NefertariLaurel <laurel_04@outlook.com>
2023-08-11 04:56:58 -0600

    pruebas

--- index.js
+++ index.js
@@ -1,7 +1,33 @@
-const duplicatedNames = (arrayNames)=> {
-    let duplicados = arrayNames.filter((value,index,array)=>{
-        return array.indexOf(value) !== index;
-    });
+/* COMENZAMOS */
 
-    return duplicados.length > 0;
+const prompt = require('prompt-sync')();
+
+let nombres = [];
+
+/* ESCRIBIR NOMBRES PARA EL CONTEO */
+
+function obtenerNombre() {
+  const nombre = prompt('Ingresa un nombre (o escribe "salir" para terminar): ');
+  if (nombre.toLowerCase() === 'salir') {
+    mostrarInformacion();
+  } else {
+    nombres.push(nombre);
+    obtenerNombre();
+  }
 }
+function mostrarInformacion() {
+  console.log(`Cantidad de nombres ingresados: ${nombres.length}`);
+
+  const nombresUnicos = [...new Set(nombres)];
+  const hayRepetidos = nombres.length !== nombresUnicos.length;
+  console.log(`Hay nombres repetidos: ${hayRepetidos ? 'Si' : 'No'}`);
+
+  const nombreMasLargo = nombres.reduce((max, nombre) => (nombre.length > max.length) ? nombre : max, '');
+  console.log(`Nombre más largo: ${nombreMasLargo}`);
+
+  const nombreMasCorto = nombres.reduce((min, nombre) => (nombre.length < min.length || min === '') ? nombre : min, '');
+  console.log(`Nombre más corto: ${nombreMasCorto}`);
+}
+
+console.log("Ingresa los nombres o Escribe 'salir' para terminar.");
+obtenerNombre();
